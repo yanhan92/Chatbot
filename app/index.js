c96fc23423d92ea96a2e3e9b6fe3ef73a6ecd3bc
Yan Han TAN <yanhan92@gmail.com>
2017-10-01 12:11:40 +0800

    Included port

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 var Tgfancy = require ('tgfancy');
 var fs = require('fs');
 var qpx = require('google-flights-api');
+var port = process.env.PORT || 8443;
+var host = process.env.HOST;
 
 const QPX_API_KEY = process.env.QPX_API_KEY;
 const options = { write: __dirname + '\\data'};
 var google_qpx = new qpx(QPX_API_KEY, options);
-var changiairbot = new Tgfancy (process.env.Telegram_API_KEY, {polling : true});
+var changiairbot = new Tgfancy ({process.env.Telegram_API_KEY, {polling : true,port: port, host: host});
 
 changiairbot.on("text", function(message){
 
