d3d43fb075d5ddb08e36b6ccf6da7e6c90d1d866
Yan Han TAN <yanhan92@gmail.com>
2017-10-01 15:56:56 +0800

    Fixed index out of bound

--- app/index.js
+++ app/index.js
@@ -72,7 +72,7 @@ changiairbot.on("text", function(message){
 			}).catch(console.error);
 		}
 		else{
-			changiairbot.sendMessage(message.chat.id , `The input \"${message.text}\" should have 3 distinct fields`);
+			changiairbot.sendMessage(message.chat.id , `The input "${message.text}" should have 3 distinct fields`);
 			changiairbot.sendMessage(message.chat.id , "Tell me the airport code of departure followed by the airport code of arrival and the date of travel.");
 			changiairbot.sendMessage(message.chat.id , `Like 'HKG SIN ${today}' `);
 		}
